54700b2973872612afcd48bbba2b39d01e195474
Craig Nelson <craig.michael.nelson@gmail.com>
2014-10-22 09:21:08 -0500

    Datalist integration
    Starting to add datalist integration

---
+++ repo-amp/src/main/java/com/redwingaero/model/VendorDatalist.java
@@ -0,0 +1,26 @@
+package com.redwingaero.model;
+
+import org.alfresco.service.namespace.QName;
+
+
+/**
+ * References to all of the items defined in valueAssistanceModel.xml
+ * 
+ */
+public class VendorDatalist
+{
+ 
+    /**Value Assistance Model URI */
+    public static final String REDWINGAERO_VALUE_ASSISTANCE_MODEL_URI = "http://www.redwingaero.com/model/rwad/1.0";
+    public static final String DATALIST_MODEL_URI = "http://www.alfresco.org/model/datalist/1.0";
+
+    public static final String REDWINGAERO_VALUE_ASSISTANCE_MODEL_PREFIX = "rwad";
+    public static final String CONTENT_MODEL_PREFIX = "cm";
+    public static final String DATALIST_MODEL_PREFIX = "dl";
+    
+    public static final QName TYPE_VALUE_ASSISTANCE_LIST_ITEM = QName.createQName(REDWINGAERO_VALUE_ASSISTANCE_MODEL_URI, "VendorDatalist");
+    public static final QName TYPE_DATALIST = QName.createQName(DATALIST_MODEL_URI, "dataList");
+    
+    public static final QName PROP_SORT_ORDER = QName.createQName(REDWINGAERO_VALUE_ASSISTANCE_MODEL_URI, "VendorId");
+    public static final QName PROP_VALUE = QName.createQName(REDWINGAERO_VALUE_ASSISTANCE_MODEL_URI, "VendorName");
+}
